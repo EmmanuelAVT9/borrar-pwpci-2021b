df2b8a7e8cca44a305a16bf9c61e757648c538fd
JuliettCarrillo <L181130077@gamadero.tecnm.mx>
2021-11-10 20:50:49 -0600

    S23 Logeando El objeto request

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 // 1. Importar el module http
 import http from 'http';
 
+//2. Crear el servidor
 const server = http.createServer((req,res)=>{
     console.log("> Se ha recibido una peticion.");
-    //respondemos
-    res.write('Hola');
-    // terminar la conexion
+    // Registrar informacion de la peticion
+    console.log(`🤜🤜 Informacion de la peticion`);
+    console.log(`🤜url: ${req.url}`);
+    console.log(`🤜Request Method: ${req.method}`);
+    console.log(`🤜Plataforma del cliente: ${req.headers["sec-ch-ua-platform"]}`);
+    // Respondemos
+    res.write("Esta es la respuesta del servidor.");
+    // Terminar la conexion
     res.end();
 });
 //
-//
+//3.Pongo a trabajar el servidor
 server.listen(3000,"192.168.56.1",()=>{
     console.log("👩‍🍳Servidor escuchando en http://192.168.56.1:3000");
 })
