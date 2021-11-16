06b19ccdc89898c047a225b0e0358593e6bd12ed
EmmanuelAVT9 <alexsoge@gmail.com>
2021-11-15 18:38:57 -0600

    S24 Enrutando 2 recursos

--- index.js
+++ index.js
@@ -3,20 +3,53 @@ import http from 'http';
 
 //2. Crear el servidor
 const server = http.createServer((req,res)=>{
+    //Obteniendo el recurso solicitado
+    let {url} = req;
+
+    //Informa en la consola del servidor que se recibe una peticion
     console.log("> Se ha recibido una peticion.");
-    // Registrar informacion de la peticion
-    console.log(`🤜🤜 Información de la peticion`);
-    console.log(`🤜url: ${req.url}`);
-    console.log(`🤜Request Method: ${req.method}`);
-    // Estableciendo el tipo de contenido que se entregara al cliente
-    res.setHeader('Content-Type', 'text/html');
-    // Envio el contenido 
-    res.write("<html>");
-    res.write("<head><title>My app</title></head>");
-    res.write(`<body><h1>Hello world from the server &#128519</h1><p style="color:red">Recurso Solicitado: ${req.url}</p></body>`);
-    res.write("</html>");
-    // Terminar la conexion
+
+    //filtrar la url
+    if(url === '/'){
+        //respuesta ante "get /"
+        //1. estableciendo el tipo de retoirno
+        // como html
+        res.setHeader('Content-Type', 'text/html');
+        // 2. escribiendo respuesta
+        res.write('<html>');
+        res.write('<head><title>My app</title></head>');
+        res.write('<body><h1>&#9889; Hello world from the server &#9889;</h1></body>');
+        res.write('</html>');
+        // Terminar la conexion
+        res.end();
+    }else if(url === '/author'){
+         //respuesta ante "get /"
+        //1. estableciendo el tipo de retoirno
+        // como html
+        res.setHeader('Content-Type', 'text/html');
+        // 2. escribiendo respuesta
+        res.write('<html>');
+        res.write('<head><title>My app</title></head>');
+        res.write('<body>');
+        res.write('<h1>&#9889; Author &#9889;</h1>');
+        res.write('<p>Emmanuel Alejandro Vazquez Trujillo</p>');
+        res.write('</body>');
+        res.write('</html>');
+        // Terminar la conexion
+        res.end();
+    }else{
+        //se registra el recurso no encontrado
+        console.log(`❌No se ha encontrado el recurso: ${url}`);
+        //recurso no encontrado
+        res.setHeader('Content-Type', 'text/html');
+        // 2. escribiendo respuesta
+        res.write("<html>");
+        res.write("<head><title>My app</title></head>");
+        res.write("<body><h1>eror 404 Recurso no encontrado: &#9940</h1></body>");
+        res.write("</html>");
+        // Terminar la conexion
     res.end();
+    }
 });
 //
 //3.Pongo a trabajar el servidor
