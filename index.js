662ed82a43fbc1a3fdc5be8250c6395535de7be5
EmmanuelAVT9 <alexsoge@gmail.com>
2021-11-26 08:41:02 -0600

    S31 Procesando un formulario

--- index.js
+++ index.js
@@ -9,11 +9,41 @@ import Express from 'express'
 // Crear una instancia de Expres
 const app = Express(); // (req, res, next)=>{} event handler
 
+// 1. Insertando Middleware para la lectura de datos
+// desde un cliente
+app.use(Express.urlencoded({extended: false}));
+
+// Loggin de peticiones
+app.use((req,_,next)=>{
+    console.log(`📞 Se ha realizado la petición:`)
+});
 // Se debe colocar primero ya que el orden de registro
 // determina el orden de verificaciòn
 app.use('/about',(_,res)=>{
     console.log('📞 Se ha realizado la petición:"/about"');
     res.send("<h1>💡 Acerca de ...</h1>\n🙋‍♂️ Sitio inicial hecho con NodeJs");
+});
+
+//Sirviendo recurso de formulario
+app.use('/add-student-form',(req, res, next)=>{
+    res.send(`
+    <form action="/add-student" method="POST">
+        <label for="student-name">👨‍🎓Student Name</label>
+        <input type="text" name="name" id="student-name">
+        <button type="submit">Add student</button>
+    </form>
+    `);
+});
+
+// Ruta que procesa el formulario
+app.use('/add-student', (req,res,next)=>{
+    // Iterando sobre todo el objeto
+    for (const prop in req.body) {
+        console.log(`🚩${prop}: ${req.body[prop]}`);
+    }
+    console.log(`🚩 Metodo: ${req.method}`);
+    // Realizamos un direccionamiento
+    res.json(req.body);
 });
 
 // La ruta raiz entra en todo tipo de peticiòn
